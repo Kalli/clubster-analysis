257d5968fb11c9cb7b46888d1fe1cef073fbf055
Karl Tryggvason <ktryggvason@gmail.com>
2020-06-18 16:16:25 +0200

    Add mobile rotation component

---
+++ src/Rotate.js
@@ -0,0 +1,23 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faSyncAlt, faMobileAlt} from "@fortawesome/free-solid-svg-icons"
+import React from "react"
+import './rotate.scss';
+
+
+function Rotate(){
+	return <div className="rotate">
+		<div>
+			<h2>
+				Please turn your phone to landscape mode to properly view
+				this webpage.
+			</h2>
+			<FontAwesomeIcon icon={faMobileAlt}/>
+			<FontAwesomeIcon icon={faSyncAlt} spin={true}/>
+			<h2>
+				Thanks!
+			</h2>
+		</div>
+	</div>
+}
+
+export default Rotate
--- src/frontPage.js
+++ src/frontPage.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import './frontPage.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
-
+import Rotate from "./Rotate"
 
 class FrontPage extends Component {
 
@@ -25,38 +25,40 @@ class FrontPage extends Component {
 	}
 
 	render(){
-
-		return <div
-			className={"frontPage"}
-            style={{height: this.state.height, width: this.state.width}}
-		>
-			<div className="jumbotron">
-				<h1 className="text-center">
-					Resident Advisor Club Communities
-				</h1>
-				<p>
-					How different or similar are the worlds biggest night clubs?
-					Which clubs rely on residents and which have djs rotate through?
-					How different are the super clubs from the underground fare?
-				</p>
-				<p>
-					In an attempt to answer these questions and others like them we looked
-					at the most recommended clubs for the top 20 regions on the
-					reputable website <a href="https://www.residentadvisor.net/events">Resident Advisor</a>.
-					We gathered all the listings for the year 2019 and then compared
-					the clubs based on djs and artists that played there.
-					Take a look to see where your favourite club fits in.
-				</p>
-				<h3>
-					Scroll down to get started!
-				</h3>
-				<div className={'scrollDown center'}>
-					<a  href="#start">
-						<FontAwesomeIcon icon={faChevronDown} />
-					</a>
+		return <>
+			<Rotate />
+			<div
+				className={"frontPage"}
+	            style={{height: this.state.height, width: this.state.width}}
+			>
+				<div className="jumbotron">
+					<h1 className="text-center">
+						Resident Advisor Club Communities
+					</h1>
+					<p>
+						How different or similar are the worlds biggest night clubs?
+						Which clubs rely on residents and which have djs rotate through?
+						How different are the super clubs from the underground fare?
+					</p>
+					<p>
+						In an attempt to answer these questions and others like them we looked
+						at the most recommended clubs for the top 20 regions on the
+						reputable website <a href="https://www.residentadvisor.net/events">Resident Advisor</a>.
+						We gathered all the listings for the year 2019 and then compared
+						the clubs based on djs and artists that played there.
+						Take a look to see where your favourite club fits in.
+					</p>
+					<h3>
+						Scroll down to get started!
+					</h3>
+					<div className={'scrollDown center'}>
+						<a  href="#start">
+							<FontAwesomeIcon icon={faChevronDown} />
+						</a>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	}
 }
 export default FrontPage
