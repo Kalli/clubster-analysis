93aaa05dbdf63bd44671d644aba8b1d41c7a70ef
mtryggvason <m.tryggvason@kiska.com>
2020-11-05 21:38:08 +0100

    add device check for pan and zoom

---
+++ src/utils/getDeviceType.js
@@ -0,0 +1,14 @@
+export default () => {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  }
+  if (
+    /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      ua
+    )
+  ) {
+    return "mobile";
+  }
+  return "desktop";
+};
